12ec3bf8f50e62b17360d3e897aaf93b01bcbe7c
user <user@local.domain>
2022-10-20 00:14:17 -0400

    update

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -39,13 +39,13 @@ require("which-key").setup({
   window = {
     border = "none", -- none, single, double, shadow
     position = "bottom", -- bottom, top
-    margin = { 0, 40, 3, 40 }, -- extra window margin [top, right, bottom, left]
+    margin = { 1, 2, 1, 2 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
   },
   layout = {
     height = { min = 3, max = 25 }, -- min and max height of the columns
     width = { min = 10, max = 50 }, -- min and max width of the columns
-    spacing = 20, -- spacing between columns
+    spacing = 5, -- spacing between columns
     align = "center", -- align columns left, center or right
   },
   ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
