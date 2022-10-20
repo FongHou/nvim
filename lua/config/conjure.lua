d5be378ba882491eae5cb5de0f8de2e4fba2b099
user <user@local.domain>
2022-10-19 23:47:20 -0400

    update

--- lua/config/conjure.lua
+++ lua/config/conjure.lua
@@ -1,8 +1,6 @@
 vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = true
-vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "nbb nrepl-server :port 8794"
+vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = true
 vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
--- vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.stdio"
--- vim.g["conjure#client#fennel#stdio#command"] = "fennel --lua luajit"
 vim.g["conjure#mapping#eval_motion"] = ","
 vim.g["conjure#mapping#eval_visual"] = ","
 vim.g["conjure#mapping#doc_word"] = "vd"
@@ -22,5 +20,25 @@ vim.api.nvim_create_autocmd("FileType", {
       t = { "test" },
       v = { "view" },
     }, { prefix = "<localleader>", mode = "n", silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.clj",
+  callback = function()
+    local port = 9876
+    vim.fn.delete(".nrepl-port")
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#port"] = port
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "bb nrepl-server localhost:" .. port
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.cljs",
+  callback = function()
+    local port = 9976
+    vim.fn.delete(".nrepl-port")
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#port"] = port
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "nbb nrepl-server :port " .. port
   end,
 })
