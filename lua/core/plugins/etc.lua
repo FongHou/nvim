e95c2fd11ee25cfe69565633e0b4337e58e99050
user <user@local.domain>
2022-12-30 13:29:55 -0500

    enable nvim-notify without noice

--- lua/core/plugins/etc.lua
+++ lua/core/plugins/etc.lua
@@ -1,9 +1,16 @@
-return {
+local M = {
   "ThePrimeagen/harpoon",
   dependencies = {
     "mbbill/undotree",
     "mg979/vim-visual-multi",
     "preservim/tagbar",
+    "rcarriga/nvim-notify",
     "tpope/vim-repeat",
   },
 }
+
+function M.config()
+  vim.notify = require("notify")
+end
+
+return M
