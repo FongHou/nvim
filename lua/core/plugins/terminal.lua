401d4d681283b8a1076a6c18f1b196644633bcff
user <user@local.domain>
2023-03-09 16:29:17 -0500

    update

--- lua/core/plugins/terminal.lua
+++ lua/core/plugins/terminal.lua
@@ -69,10 +69,10 @@ function M.config()
   local set_terminal_keymaps = function()
     local opts = { noremap = true }
     buf_map(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    buf_map(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    buf_map(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    buf_map(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    buf_map(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    buf_map(0, "t", "<M-h>", [[<C-\><C-n><C-W>h]], opts)
+    buf_map(0, "t", "<M-j>", [[<C-\><C-n><C-W>j]], opts)
+    buf_map(0, "t", "<M-k>", [[<C-\><C-n><C-W>k]], opts)
+    buf_map(0, "t", "<M-l>", [[<C-\><C-n><C-W>l]], opts)
   end
   -- if you only want these mappings for toggle term use term://*toggleterm#* instead
   vim.api.nvim_create_autocmd("TermOpen", {
