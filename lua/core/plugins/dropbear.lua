dec71db5c6fa58214341baf7ffd9f9aacd81b555
user <user@local.domain>
2023-07-04 11:32:00 -0400

    update

--- lua/core/plugins/dropbear.lua
+++ lua/core/plugins/dropbear.lua
@@ -3,5 +3,5 @@ local utils = require("core.utils.functions")
 return {
   "Bekaboo/dropbar.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  enabled = utils.isNeovimVersionsatisfied(10),
+  enabled = false,
 }
